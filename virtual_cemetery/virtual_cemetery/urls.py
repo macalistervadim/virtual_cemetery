bf94a4f0beedf1cf3e48bec1b0189720e121fba3
macalistervadim <macalistervadim@yandex.ru>
2024-04-02 21:30:00 +0600

    base commit

--- virtual_cemetery/virtual_cemetery/urls.py
+++ virtual_cemetery/virtual_cemetery/urls.py
@@ -4,3 +4,15 @@ import django.urls
 urlpatterns = [
     django.urls.path("admin/", django.contrib.admin.site.urls),
 ]
+
+if django.conf.settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += (
+        [
+            django.urls.path(
+                "__debug__/",
+                django.urls.include(debug_toolbar.urls),
+            ),
+        ]
+    )
