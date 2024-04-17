3bcd97bfedf97f712297ddc2908c03ab52a605d3
kiMtals <kitagr2007@gmail.com>
2024-04-17 21:42:14 +0700

    nice_header

---
+++ virtual_cemetery/static_dev/js/header.js
@@ -0,0 +1,14 @@
+let header = document.querySelector(".js-header")
+headerHeight = document.querySelector(".js-header").clientHeight
+
+document.onscroll = function (){
+    let scroll = window.scrollY;
+
+    if (scroll > headerHeight + 113){
+        header.classList.add("fixed");
+        document.body.style.paddingTop = headerHeight + "px";
+    } else{
+        header.classList.remove("fixed");
+        document.body.removeAttribute("style")
+    }
+}
